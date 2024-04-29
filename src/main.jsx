1c9d4e379decb3de6f86faa750495dde37ff4c0a
imambahrulgausi <162246480+imambahrulgausi@users.noreply.github.com>
2024-04-29 10:44:08 +0700

    Frontend tugas 3

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Induk from './Pages/Induk.jsx'
+import App from './App.jsx'
+import LandingPage from './Pages/LandingPage.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <LandingPage />
+  </React.StrictMode>,
+)
